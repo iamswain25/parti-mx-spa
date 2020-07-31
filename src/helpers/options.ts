aedbac81d1336a23bfb2d6270bbf522d07d163c0
Swain Hwang <swain@swainuicbookpro.lan>
2020-07-31 01:45:00 -0700

    30일 후 종료 옵션 추가

--- src/helpers/options.ts
+++ src/helpers/options.ts
@@ -7,11 +7,14 @@ export const postSortOptions = [
 ];
 export const voteOptions = [
   { label: "7일 후 종료", value: "7days" },
-  { label: "3일 후 종료", value: "3days" },
+  { label: "30일 후 종료", value: "3days" },
   { label: "토론 정리시 종료", value: "manual" },
 ];
 
-export const suggestionOptions = [{ label: "30일 후 종료", value: "30days" }];
+export const suggestionOptions = [
+  { label: "7일 후 종료", value: "7days" },
+  { label: "30일 후 종료", value: "30days" },
+];
 
 export const userGroupStatusList = [
   { label: "운영자", value: "organizer" },
