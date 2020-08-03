87809ec56730d5f6567dec08405606e77533d6a3
Swain Hwang <swain@swainuicbookpro.lan>
2020-08-02 19:18:44 -0700

    param으로 그룹 접속

--- src/components/useParseGroupId.ts
+++ src/components/useParseGroupId.ts
@@ -3,16 +3,16 @@ import { useLocation } from "react-router-dom";
 import { useStore } from "../store/store";
 
 export default function useParseGroupId() {
-  const [, dispatch] = useStore();
+  const [{ isInit }, dispatch] = useStore();
   const { search } = useLocation();
   const params = new URLSearchParams(search);
   const groupId = params.get("group_id");
   // console.log(groupId, search);
   React.useEffect(() => {
-    if (groupId) {
+    if (groupId && isInit) {
       const group_id = Number(groupId);
       dispatch({ type: "SET_GROUP", group_id });
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [groupId]);
+  }, [groupId, isInit]);
 }
