4e14d7287a315e37d6b6d15094532ad2683db742
swain <iamswain@hotmail.com>
2020-08-06 20:09:32 -0700

    fix: comment delete error

--- src/components/useCommentDelete.ts
+++ src/components/useCommentDelete.ts
@@ -16,17 +16,18 @@ export default (id: number) => {
   async function handler() {
     if (window.confirm("댓글을 삭제하시겠습니까?")) {
       let parent_id = null;
+      let parent_body = null;
       try {
         const res = await remove();
-        parent_id = res.data.delete_mx_comments_by_pk.parent_id;
-
+        parent_id = res.data.delete_mx_comments_by_pk.parent.id;
+        parent_body = res.data.delete_mx_comments_by_pk.parent.body;
         setSuccess("댓글을 삭제 했습니다");
       } catch (error) {
         await nullify();
       }
       try {
         // 상위 댓글이 있을 경우 지우려고 시도
-        if (parent_id !== null) {
+        if (parent_id && !parent_body) {
           await remove({ variables: { comment_id: parent_id } });
         }
       } catch (error) {
