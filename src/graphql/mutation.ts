d4c14fa87dd7a92bc81977408180782b2679d5db
swain <iamswain@hotmail.com>
2020-08-13 19:06:20 -0700

    join as user

--- src/graphql/mutation.ts
+++ src/graphql/mutation.ts
@@ -203,7 +203,7 @@ export const unlikeComment = gql`
 export const insertUserGroup = gql`
   mutation($group_id: Int!) {
     insert_mx_users_group_one(
-      object: { group_id: $group_id, status: "requested" }
+      object: { group_id: $group_id, status: "user" }
       on_conflict: { update_columns: [status], constraint: users_group_pkey }
     ) {
       user_id
