b6d7584c9cf99f55adcf86ce2c478b7a1d56232f
swain <iamswain@hotmail.com>
2020-10-27 10:34:17 +0900

    notice edit

--- src/components/NoticeEdit.tsx
+++ src/components/NoticeEdit.tsx
@@ -1,19 +1,21 @@
 import React from "react";
-
 import { useForm } from "react-hook-form";
 import { Container, Typography, Box, Hidden } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
 import HeaderNew from "./HeaderNew";
-
 import { Post, Img, File as File2, NoticeFormdata } from "../types";
 import SavedImageFile from "./SavedImageFile";
 import BtnSubmitDesktop from "./BtnSubmitDesktop";
 import ImageFileDropzone from "./ImageFileDropzone";
 import NoticeInput from "./NoticeInput";
+import { makeUpdateVariables } from "./makePostVariables";
+import { firestore } from "../config/firebase";
+import { useSuccess } from "../store/useGlobalState";
 
 export default function NoticeEdit({ post: p }: { post: Post }) {
   const { id, title, body, files, images, html } = p;
   const history = useHistory();
+  const [, setSuccess] = useSuccess();
   const [imageArr, setImageArr] = React.useState<File[]>([]);
   const [fileArr, setFileArr] = React.useState<File[]>([]);
   const [images2, setImages2] = React.useState<Img[] | undefined>(images);
@@ -22,49 +24,41 @@ export default function NoticeEdit({ post: p }: { post: Post }) {
     defaultValues: { title, body, html, isHtml: !!html },
   });
   const { handleSubmit } = formControl;
-
   async function handleForm(form: NoticeFormdata) {
-    // const variables = await makeUpdateVariables(form, {
-    //   imageArr,
-    //   fileArr,
-    //   images2,
-    //   files2,
-    //   setSuccess,
-    //   id,
-    // });
-    // const res = await update({
-    //   variables,
-    // });
-    // console.log(res);
+    const variables = await makeUpdateVariables(form, {
+      imageArr,
+      fileArr,
+      images2,
+      files2,
+      setSuccess,
+    });
+    await firestore.collection("posts").doc(id).update(variables);
     history.push("/post/" + id);
   }
-
   return (
-    <>
-      <form onSubmit={handleSubmit(handleForm)} noValidate autoComplete="off">
-        <Hidden mdUp>
-          <HeaderNew title="소식 수정" />
-        </Hidden>
-        <Box mt={2}>
-          <Container component="main" maxWidth="md">
-            <Typography variant="h2">소식 수정</Typography>
-            <NoticeInput formControl={formControl} />
-            <ImageFileDropzone
-              images={imageArr}
-              setImages={setImageArr}
-              files={fileArr}
-              setFiles={setFileArr}
-            />
-            <SavedImageFile
-              files={files2}
-              images={images2}
-              setFiles={setFiles2}
-              setImages={setImages2}
-            />
-            <BtnSubmitDesktop text="소식 수정" />
-          </Container>
-        </Box>
-      </form>
-    </>
+    <form onSubmit={handleSubmit(handleForm)} noValidate autoComplete="off">
+      <Hidden mdUp>
+        <HeaderNew title="소식 수정" />
+      </Hidden>
+      <Box mt={2}>
+        <Container component="main" maxWidth="md">
+          <Typography variant="h2">소식 수정</Typography>
+          <NoticeInput formControl={formControl} />
+          <ImageFileDropzone
+            images={imageArr}
+            setImages={setImageArr}
+            files={fileArr}
+            setFiles={setFileArr}
+          />
+          <SavedImageFile
+            files={files2}
+            images={images2}
+            setFiles={setFiles2}
+            setImages={setImages2}
+          />
+          <BtnSubmitDesktop text="소식 수정" />
+        </Container>
+      </Box>
+    </form>
   );
 }
