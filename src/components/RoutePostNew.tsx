07dd36e34a2231d6564c3e3b0031d093b1552abc
swain <iamswain@hotmail.com>
2020-10-29 11:31:38 +0900

    오거나이저 아닌 패널도 소식 글쓰기 가능

--- src/components/RoutePostNew.tsx
+++ src/components/RoutePostNew.tsx
@@ -32,10 +32,6 @@ export default function RoutePostNew() {
   if (!b) {
     return <Forbidden />;
   }
-  if (b.type === "notice" && userStatus !== "organizer") {
-    // 오거나이저만 소식 게시 가능
-    return <Forbidden />;
-  }
   if (permissionBlocked(b.permission, userStatus)) {
     return <Forbidden />;
   }
