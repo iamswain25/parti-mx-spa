498d216f86b33d010830fa5555088c981844b43d
swain <iamswain@hotmail.com>
2020-11-03 14:18:03 +0900

    footer 순서 홈피와 같게

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,9 @@ const useStyles = makeStyles((theme) => ({
   root: {
     margin: theme.spacing(5),
     "& a": {
+      // "&:first-child": {
+      //   marginLeft: theme.spacing(0),
+      // },
       color: theme.palette.primary.main,
       marginLeft: theme.spacing(1),
       marginRight: theme.spacing(1),
@@ -12,6 +15,11 @@ const useStyles = makeStyles((theme) => ({
       display: "flex",
       alignItems: "center",
       justifyContent: "center",
+      // justifyContent: "flex-start",
+      "&.bold": {
+        fontWeight: "bold",
+        marginTop: theme.spacing(3),
+      },
     },
   },
 }));
@@ -20,29 +28,30 @@ export default function Footer() {
   return (
     <footer className={classes.root}>
       <div>
-        Copyright ©
-        <a href="https://ywca.or.kr/" target="_blank" rel="noopener noreferrer">
-          한국YWCA연합회
-        </a>
-        All Rights Reserved.
-      </div>
-      <div>
-        <a
-          href="mailto:peacenow@ywca.or.kr"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          email: peacenow@ywca.or.kr
-        </a>
-      </div>
-      <div>주소 : 04538 서울시 중구 명동길 73(명동 1가)</div>
-      <div>
         <a href="/" target="_blank" rel="noopener noreferrer">
           이용약관
         </a>
         <a href="/" target="_blank" rel="noopener noreferrer">
           개인정보취급방침
         </a>
+      </div>
+      <div>서울특별시 중구 퇴계로 18길 77(남산동 2가 45-6)</div>
+      <div>TEL 02-774-9702-7 | FAX 02-774-9724</div>
+      <div>
+        <a
+          href="mailto:peacenow@ywca.or.kr"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          이메일 peacenow@ywca.or.kr
+        </a>
+      </div>
+      <div className="bold">
+        Copyright 2020
+        <a href="https://ywca.or.kr/" target="_blank" rel="noopener noreferrer">
+          한국YWCA연합회
+        </a>
+        All Rights Reserved.
       </div>
     </footer>
   );
