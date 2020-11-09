ed2cf38091415c3bb76a5a7c2abf252a52ad7717
swain <iamswain@hotmail.com>
2020-11-09 14:43:45 +0900

    진행/완료 제보 선택란 없애기

--- src/components/RouteBoardSuggestion.tsx
+++ src/components/RouteBoardSuggestion.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Board } from "../types";
 import { makeStyles } from "@material-ui/core/styles";
 import { grey } from "@material-ui/core/colors";
-import { Typography, Grid, Box, Button } from "@material-ui/core";
 import RoutePostSuggestion from "./RoutePostSuggestion";
 import usePosts from "../store/usePosts";
 const useStyles = makeStyles((theme) => {
@@ -28,52 +27,12 @@ const useStyles = makeStyles((theme) => {
 
 export default function RouteBoardSuggestion({ board: b }: { board: Board }) {
   const classes = useStyles();
-  const [isClosed, setClosed] = React.useState(false);
   const [posts] = usePosts({ board_id: b.id });
   return (
     <section className={classes.container}>
-      <Grid container alignItems="center" className={classes.titleContainer}>
-        <Button onClick={() => setClosed(false)} style={{ padding: 0 }}>
-          <Box display="flex" alignItems="center">
-            <Typography
-              variant="h4"
-              color={isClosed ? "textSecondary" : "textPrimary"}
-            >
-              진행 중인 제보
-            </Typography>
-            <Box mr={1} />
-            <Typography
-              variant="h4"
-              color={isClosed ? "textSecondary" : "primary"}
-            >
-              {b.count_open}
-            </Typography>
-          </Box>
-        </Button>
-        <Box mr={2} />
-        <Button onClick={() => setClosed(true)} style={{ padding: 0 }}>
-          <Box display="flex" alignItems="center">
-            <Typography
-              variant="h4"
-              color={isClosed ? "textPrimary" : "textSecondary"}
-            >
-              완료 된 제보
-            </Typography>
-            <Box mr={1} />
-            <Typography
-              variant="h4"
-              color={isClosed ? "primary" : "textSecondary"}
-            >
-              {b.count_closed}
-            </Typography>
-          </Box>
-        </Button>
-      </Grid>
-      {posts
-        .filter((a) => (typeof a.closed_at === "string") === isClosed)
-        .map((p, i) => (
-          <RoutePostSuggestion key={p.id} post={p} />
-        ))}
+      {posts.map((p) => (
+        <RoutePostSuggestion key={p.id} post={p} />
+      ))}
     </section>
   );
 }
