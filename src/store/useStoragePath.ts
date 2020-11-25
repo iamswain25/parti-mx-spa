5ca2342882cfeac5aaf47c2ce1630a6cff61a2cd
swain <iamswain@hotmail.com>
2020-11-25 11:09:44 +0900

    fix: show data:image

--- src/store/useStoragePath.ts
+++ src/store/useStoragePath.ts
@@ -6,7 +6,14 @@ export default function useStoragePath(path?: string) {
   React.useEffect(() => {
     // console.log(path);
     if (path) {
-      if (path?.startsWith("/") || path?.startsWith("blob:")) {
+      console.log(path);
+      if (
+        path?.startsWith("/") ||
+        path?.startsWith("blob:") ||
+        path?.startsWith("https://") ||
+        path?.startsWith("http://") ||
+        path?.startsWith("data:image/")
+      ) {
         setUrl(path);
       } else {
         storage.ref(path).getDownloadURL().then(setUrl);
