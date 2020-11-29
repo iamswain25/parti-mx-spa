051e6b6f726a13eb69b0b1c0efbebe6b54f35a9a
swain <iamswain@hotmail.com>
2020-11-30 03:49:03 +0900

    #46 공지 게시판 글 작성자 오류 수정

--- src/components/BoardPostSub2.tsx
+++ src/components/BoardPostSub2.tsx
@@ -3,18 +3,17 @@ import { Post } from "../types";
 import { semanticDate } from "../helpers/datefns";
 import { Typography, Box } from "@material-ui/core";
 import usePostCounter from "../store/usePostCounter";
+import useUser from "../store/useUser";
 export default function BoardPostSub2({ post: p }: { post: Post }) {
   const [counter] = usePostCounter(p.id);
+  const [user] = useUser({ id: p.created_by });
   const { count_comment = 0, count_like = 0 } = counter || {};
   const isVote = p.type === "vote";
-  const name = p?.name;
-  const alteredName =
-    name?.indexOf("@") > 0 ? name?.substr(0, name?.indexOf("@")) : name;
   return (
     <Box color="grey.600">
       <Typography variant="body2">
         <Box component="span" mr={1}>
-          {alteredName || "익명"}
+          {user?.name || "익명"}
         </Box>
         <Box component="span" mr={1}>
           {semanticDate(p.created_at)}
