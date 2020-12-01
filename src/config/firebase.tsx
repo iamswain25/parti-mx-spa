394c8c84a6c1119a5e2fbf343152baacfd3e64a1
DK <20438637+jenna1k@users.noreply.github.com>
2020-12-01 10:49:54 +0900

    replaced project name

--- src/config/firebase.tsx
+++ src/config/firebase.tsx
@@ -1,5 +1,5 @@
 import firebase from "firebase/app";
-import { policyFair as firebaseConfig } from "./firebaseConfig";
+import { gyeonggiVillage as firebaseConfig } from "./firebaseConfig";
 import "firebase/analytics";
 import "firebase/auth";
 import "firebase/functions";
