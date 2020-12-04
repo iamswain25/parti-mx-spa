f897603bd33366d121cb4a9320f2fba53eeac846
swain <iamswain@hotmail.com>
2020-12-04 18:12:41 +0900

    ie11

---
+++ src/components/Ie11.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import "./Ie11.sass";
+export default function Ie11() {
+  return (
+    <section id="ie11">
+      <div className="container">
+        <div className="title">
+          제19회 전국주민자치박람회 웹사이트를 방문해주셔서 감사합니다.
+        </div>
+        <div className="label">
+          지원하지 않는 브라우저로 접속 시 이용이 제한될 수 있습니다.
+          <br />
+          웹사이트에 접속하려면 다른 브라우저를 이용하세요.
+          <br />
+          제19회 전국주민자치박람회 웹사이트는 Chrome, Microsoft Edge에
+          최적화되어 있습니다.
+          <br />
+        </div>
+        <div className="images">
+          <a
+            href="https://www.google.com/intl/ko/chrome/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="square"
+          >
+            <img src="/logo-chrom.png" alt="chrome" className="image" />
+            <div className="flex-center">
+              <span className="chrome">Chrome</span>
+              <span>설치하기</span>
+            </div>
+          </a>
+          <a
+            href="https://www.microsoft.com/ko-kr/edge"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="square"
+          >
+            <img src="/logo-edge.png" alt="edge" className="image" />
+            <div className="flex-center">
+              <span className="edge">Edge</span>
+              <span>설치하기</span>
+            </div>
+          </a>
+        </div>
+        <div className="text-center">
+          <a
+            href="https://pf.kakao.com/_DslxnK"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="ask"
+          >
+            문의하기
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+/** @jsxRuntime classic */
 import "react-app-polyfill/ie11";
 import "react-app-polyfill/stable";
 import React from "react";
@@ -5,8 +6,29 @@ import ReactDOM from "react-dom";
 import "./index.sass";
 import * as serviceWorker from "./serviceWorker";
 import App from "./App";
+import Ie11 from "./components/Ie11";
+function detectIE() {
+  const ua = window.navigator.userAgent;
 
-ReactDOM.render(<App />, document.getElementById("root"));
+  const msie = ua.indexOf("MSIE ");
+  if (msie > 0) {
+    // IE 10 or older => return version number
+    return parseInt(ua.substring(msie + 5, ua.indexOf(".", msie)), 10);
+  }
+
+  const trident = ua.indexOf("Trident/");
+  if (trident > 0) {
+    // IE 11 => return version number
+    const rv = ua.indexOf("rv:");
+    return parseInt(ua.substring(rv + 3, ua.indexOf(".", rv)), 10);
+  }
+  // other browser
+  return false;
+}
+ReactDOM.render(
+  detectIE() ? <Ie11 /> : <App />,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
