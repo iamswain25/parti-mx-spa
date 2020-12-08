02cd4dd01713f7d256a398583faeee0e4f1bf894
DK <20438637+jenna1k@users.noreply.github.com>
2020-12-08 17:34:20 +0900

    useEffectAuth on App & removed redirection

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import Routes from "./config/Routes";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/core";
 import { theme } from "./config/ThemeCustom";
+import useEffectAuth from "./store/useEffectAuth";
 export default function App() {
+  useEffectAuth();
   return (
     <ThemeProvider theme={theme}>
       <Router>
