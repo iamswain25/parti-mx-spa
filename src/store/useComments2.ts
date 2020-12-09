6afb16e0fb470406db08ad48930b017019060bce
swain <iamswain@hotmail.com>
2020-12-09 14:26:56 +0900

    대댓글 디폴트 get()

--- src/store/useComments2.ts
+++ src/store/useComments2.ts
@@ -1,25 +1,44 @@
 import React from "react";
 import { firestore } from "../config/firebase";
 import { Comment } from "../types";
-export default function useComments2(c: Comment) {
+export default function useComments2(c: Comment, listen = false) {
   const [items, setItems] = React.useState<Comment[]>([] as Comment[]);
   React.useEffect(() => {
     if (c.post_id && c.id)
-      return firestore
-        .collection("posts")
-        .doc(c.post_id)
-        .collection("comments")
-        .doc(c.id)
-        .collection("comments")
-        .orderBy("created_at", "asc")
-        .onSnapshot((snapshot) =>
-          setItems(
-            snapshot.docs.map(
-              (doc) =>
-                ({ id: doc.id, parent_id: c.id, ...doc.data() } as Comment)
+      if (listen) {
+        return firestore
+          .collection("posts")
+          .doc(c.post_id)
+          .collection("comments")
+          .doc(c.id)
+          .collection("comments")
+          .orderBy("created_at", "asc")
+          .onSnapshot((snapshot) =>
+            setItems(
+              snapshot.docs.map(
+                (doc) =>
+                  ({ id: doc.id, parent_id: c.id, ...doc.data() } as Comment)
+              )
             )
-          )
-        );
-  }, [c, setItems]);
+          );
+      } else {
+        firestore
+          .collection("posts")
+          .doc(c.post_id)
+          .collection("comments")
+          .doc(c.id)
+          .collection("comments")
+          .orderBy("created_at", "asc")
+          .get()
+          .then((snapshot) =>
+            setItems(
+              snapshot.docs.map(
+                (doc) =>
+                  ({ id: doc.id, parent_id: c.id, ...doc.data() } as Comment)
+              )
+            )
+          );
+      }
+  }, [c, setItems, listen]);
   return [items];
 }
