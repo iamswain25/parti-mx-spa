28490491241c35d57f9978a75a95d4d8a9fc9c43
swain <iamswain@hotmail.com>
2020-12-09 16:47:37 +0900

    응원인원도 더보기 추가

--- src/components/LikedUsers.tsx
+++ src/components/LikedUsers.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { Post } from "../types";
-import { Box, Divider, Grid } from "@material-ui/core";
+import { Box, Button, Divider, Grid, LinearProgress } from "@material-ui/core";
 import AvatarNameDate2 from "./AvatarNameDate2";
 import usePostLikedUsers from "../store/usePostLikedUsers";
-
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
 export default function LikedUsers({ post: p }: { post: Post }) {
-  const [likedUsers] = usePostLikedUsers(p.id);
+  const [likedUsers, loadmore, loading, hasMore] = usePostLikedUsers(p.id);
   return (
     <div>
       <Box my={2}>
         <Divider />
       </Box>
+      {hasMore && (
+        <Box my={2} display="flex" justifyContent="center">
+          <Button variant="outlined" onClick={loadmore}>
+            더보기
+            <ExpandLessIcon />
+          </Button>
+        </Box>
+      )}
+      {loading && <LinearProgress />}
       <Grid container wrap="wrap" spacing={2}>
         {likedUsers?.map((u) => (
           <AvatarNameDate2 key={u.id} postLike={u} />
--- src/store/usePostLikedUsers.ts
+++ src/store/usePostLikedUsers.ts
@@ -1,26 +1,94 @@
 import React from "react";
 import { firestore } from "../config/firebase";
 import { PostLike } from "../types";
-import { useCurrentUser } from "./useGlobalState";
-
-export default function usePostLikedUsers(id: string) {
-  const [items, setItems] = React.useState<PostLike[]>([] as PostLike[]);
-  const [currentUser] = useCurrentUser();
+let lastSnapshot: null | firebase.firestore.DocumentSnapshot = null;
+export default function usePostLikedUsers(
+  post_id: string
+): [PostLike[], () => void, boolean, boolean] {
+  const [items, setItems] = React.useState<PostLike[]>([]);
+  const [loading, setLoading] = React.useState<boolean>(true);
+  const [hasMore, setHasMore] = React.useState<boolean>(true);
+  const [startAfter, setStartAfter] = React.useState<
+    null | firebase.firestore.DocumentSnapshot | undefined
+  >(undefined);
   React.useEffect(() => {
-    if (currentUser) {
-      return firestore
+    if (startAfter === undefined) {
+      return;
+    }
+    let query = firestore
+      .collection("posts")
+      .doc(post_id)
+      .collection("likes")
+      .orderBy("created_at", "asc");
+    if (startAfter) {
+      query = query.startAfter(startAfter);
+    }
+    return query.onSnapshot((snapshot) => {
+      const newItems = snapshot
+        .docChanges()
+        .filter((change) => change.type === "added")
+        .map((change) => {
+          const doc = change.doc;
+          return { id: doc.id, ...doc.data() } as PostLike;
+        });
+      setItems((items) => [...items, ...newItems]);
+      const deletedIds = snapshot
+        .docChanges()
+        .filter((change) => change.type === "removed")
+        .map((change) => change.doc.id);
+      setItems((items) => [
+        ...items.filter((item) => !deletedIds.includes(item.id)),
+      ]);
+    });
+  }, [post_id, startAfter]);
+  React.useEffect(() => {
+    firestore
+      .collection("posts")
+      .doc(post_id)
+      .collection("likes")
+      .orderBy("created_at", "asc")
+      .limitToLast(12)
+      .get()
+      .then((snapshot) => {
+        if (snapshot.empty) {
+          setStartAfter(null);
+        } else {
+          lastSnapshot = snapshot.docs[0];
+          const getLast = snapshot.docs[snapshot.docs.length - 1];
+          setStartAfter(getLast);
+          const items = snapshot.docs.map(
+            (doc) => ({ id: doc.id, ...doc.data() } as PostLike)
+          );
+          setItems(items);
+        }
+        setLoading(false);
+      });
+  }, [post_id, setStartAfter]);
+  const loadmore = React.useCallback(
+    function loadmore() {
+      setLoading(true);
+      firestore
         .collection("posts")
-        .doc(id)
+        .doc(post_id)
         .collection("likes")
         .orderBy("created_at", "asc")
-        .onSnapshot((snapshot) =>
-          setItems(
-            snapshot.docs.map(
+        .endBefore(lastSnapshot)
+        .limitToLast(12)
+        .get()
+        .then((snapshot) => {
+          if (snapshot.empty) {
+            setHasMore(false);
+          } else {
+            lastSnapshot = snapshot.docs[0];
+            const newItems = snapshot.docs.map(
               (doc) => ({ id: doc.id, ...doc.data() } as PostLike)
-            )
-          )
-        );
-    }
-  }, [currentUser, setItems, id]);
-  return [items];
+            );
+            setItems((items) => [...newItems, ...items]);
+          }
+          setLoading(false);
+        });
+    },
+    [setLoading, setItems, setHasMore, post_id]
+  );
+  return [items, loadmore, loading, hasMore];
 }
