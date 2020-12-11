07a86cc7902b66d64bbce2d0e3f27a61329feda1
swain <iamswain@hotmail.com>
2020-12-11 15:14:04 +0900

    공유문구 수정

--- src/components/Ie11.tsx
+++ src/components/Ie11.tsx
@@ -5,15 +5,16 @@ export default function Ie11() {
     <section id="ie11">
       <div className="container">
         <div className="title">
-          제19회 전국주민자치박람회 웹사이트를 방문해주셔서 감사합니다.
+          2020 경기도마을공동체지원센터 어울림축제 웹사이트를 방문해주셔서
+          감사합니다.
         </div>
         <div className="label">
           지원하지 않는 브라우저로 접속 시 이용이 제한될 수 있습니다.
           <br />
           웹사이트에 접속하려면 다른 브라우저를 이용하세요.
           <br />
-          제19회 전국주민자치박람회 웹사이트는 Chrome, Microsoft Edge에
-          최적화되어 있습니다.
+          2020 경기도마을공동체지원센터 어울림축제 웹사이트는 Chrome, Microsoft
+          Edge에 최적화되어 있습니다.
           <br />
         </div>
         <div className="images">
