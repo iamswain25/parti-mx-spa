4246a6bff5e1f15adc20ba647e54f516df134ca0
swain <iamswain@hotmail.com>
2020-12-15 13:52:29 +0900

    #69 그룹만들기 후 취소가능

--- src/helpers/createGroup.ts
+++ src/helpers/createGroup.ts
@@ -2,9 +2,11 @@ import { firestore } from "../config/firebase";
 
 export default function createGroup(group_id: string) {
   const title = window.prompt("그룹명을 입력해 주세요");
-  firestore
-    .collection("groups")
-    .doc(group_id)
-    .set({ created_at: new Date(), title })
-    .catch(window.alert);
+  if (title) {
+    firestore
+      .collection("groups")
+      .doc(group_id)
+      .set({ created_at: new Date(), title })
+      .catch(window.alert);
+  }
 }
