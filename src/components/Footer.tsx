cb3cf4ba9d84481f873a2ec03a6db90e7ffbd33d
swain <iamswain@hotmail.com>
2020-12-19 11:52:59 +0900

    서사경로고에 링크 넣기

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import partiLogo from "../assets/images/logo-parti-light.png";
 import logo from "../assets/images/logo-sehub.png";
 import React from "react";
 import useDesktop from "./useDesktop";
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   bgColor: {
     backgroundColor: theme.palette.grey[700],
   },
@@ -54,7 +54,7 @@ const useStyles = makeStyles((theme) => ({
     "& .mt": {
       marginTop: theme.spacing(2),
     },
-  }
+  },
 }));
 
 export default function Footer() {
@@ -63,39 +63,63 @@ export default function Footer() {
   return (
     <footer className={classes.bgColor}>
       <section className={classes.root}>
-      <Grid container spacing={isDesktop ? 4 : 0}>
-        <Grid item sm={8} container direction="column">
-          <Grid item>
-            <img src={logo} alt="footer1" />
-          </Grid>
-          <Grid spacing={3} container className="mt">
+        <Grid container spacing={isDesktop ? 4 : 0}>
+          <Grid item sm={8} container direction="column">
             <Grid item>
-              <a href="https://docs.google.com/file/d/1HH1hSBgpl2tX266EfsjsUJvs7KAL_lctZKJnGAsTCNE/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="bold">
-                이용약관
+              <a
+                href="https://sehub.net"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={logo} alt="서사경홈페이지" />
               </a>
             </Grid>
+            <Grid spacing={3} container className="mt">
+              <Grid item>
+                <a
+                  href="https://docs.google.com/file/d/1HH1hSBgpl2tX266EfsjsUJvs7KAL_lctZKJnGAsTCNE/view?usp=sharing"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="bold"
+                >
+                  이용약관
+                </a>
+              </Grid>
+              <Grid item>
+                <a
+                  href="https://docs.google.com/file/d/11FSUnqoNijw9yGbgQCT38_fKrYS0tyTTYOOfLCVBbX8/view?usp=sharing"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="bold"
+                >
+                  개인정보 처리방침
+                </a>
+              </Grid>
+            </Grid>
+            <Grid item className="mt">
+              서울특별시 은평구 통일로 684 미래청 1층
+            </Grid>
             <Grid item>
-              <a href="https://docs.google.com/file/d/11FSUnqoNijw9yGbgQCT38_fKrYS0tyTTYOOfLCVBbX8/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="bold">
-                개인정보 처리방침
-              </a>
+              Tel: <a href="tel:+82-2-353-3553">02-353-3553</a>
+            </Grid>
+            <Grid item>
+              Fax: <a href="fax:+82-2-383-3553">02-383-3553</a>
+            </Grid>
+            <Grid item>
+              E-mail: <a href="mailto:info@sehub.net">info@sehub.net</a>
             </Grid>
           </Grid>
-          <Grid item className="mt">서울특별시 은평구 통일로 684 미래청 1층</Grid>
-          <Grid item>Tel: <a href="tel:+82-2-353-3553">02-353-3553</a></Grid>
-          <Grid item>Fax: <a href="fax:+82-2-383-3553">02-383-3553</a></Grid>
-          <Grid item>E-mail: <a href="mailto:info@sehub.net">info@sehub.net</a></Grid>
+          <Grid item xs={12} className="powered">
+            <a
+              href="https://parti.coop"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <strong>powered by</strong>
+              <img src={partiLogo} alt="parti logo" />
+            </a>
+          </Grid>
         </Grid>
-        <Grid item xs={12} className="powered">
-          <a
-          href="https://parti.coop"
-          target="_blank"
-          rel="noopener noreferrer"
-          >
-            <strong>powered by</strong>
-            <img src={partiLogo} alt="parti logo" />
-          </a>
-        </Grid>
-      </Grid>
       </section>
     </footer>
   );
