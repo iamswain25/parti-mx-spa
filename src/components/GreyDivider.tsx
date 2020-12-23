b7b0264999059535a4ba51afeb53a523114905a9
swain <iamswain@hotmail.com>
2020-12-23 14:37:18 +0900

    CHORES: type 정리

--- src/components/GreyDivider.tsx
+++ src/components/GreyDivider.tsx
@@ -1,15 +1,19 @@
-import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { grey } from "@material-ui/core/colors";
-const useStyles = makeStyles((theme) => {
+import { Theme } from "@material-ui/core";
+import React from "react";
+const useStyles = makeStyles<Theme, { height: number }>((theme) => {
   return {
-    greyBg: {
+    greyBg: ({ height = 1 }) => ({
+      [theme.breakpoints.up("md")]: {
+        display: "none",
+      },
       backgroundColor: grey[300],
-      paddingTop: (height: number) => theme.spacing(height),
-    },
+      paddingTop: height,
+    }),
   };
 });
 export default function GreyDivider({ height = 1 }) {
-  const classes = useStyles(height);
+  const classes = useStyles({ height });
   return <hr className={classes.greyBg} />;
 }
