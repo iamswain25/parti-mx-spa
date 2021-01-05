6b8188f8f9b97b41fd8a6c6bd9c7eaf45db0a642
swain <iamswain@hotmail.com>
2021-01-05 18:00:31 +0900

    데스크탑에서도 미리보기 이미지 없애기

--- src/components/BoardPostNotice.tsx
+++ src/components/BoardPostNotice.tsx
@@ -5,7 +5,6 @@ import { grey } from "@material-ui/core/colors";
 import { Typography, Box, Hidden } from "@material-ui/core";
 import BoardPostSub2 from "./BoardPostSub2";
 import { Link } from "react-router-dom";
-import useStoragePath from "../store/useStoragePath";
 import useDesktop from "./useDesktop";
 const useStyles = makeStyles((theme) => {
   return {
@@ -28,37 +27,14 @@ const useStyles = makeStyles((theme) => {
       marginBottom: theme.spacing(1),
       overflow: "hidden",
     },
-    img: {
-      display: "block",
-      overflow: "hidden",
-      [theme.breakpoints.down("sm")]: {
-        display: "none",
-      },
-      [theme.breakpoints.up("md")]: {
-        float: "left",
-        marginRight: 18,
-        "&>img": {
-          width: 176,
-          height: 120,
-          objectFit: "cover",
-          backgroundColor: grey[200],
-        },
-      },
-    },
   };
 });
 
 export default function BoardPostNotice({ post: p }: { post: Post }) {
   const classes = useStyles();
   const [isDesktop] = useDesktop();
-  const firstImage = useStoragePath(p.images?.[0]?.path);
   return (
     <div className={classes.container}>
-      {firstImage && (
-        <Link to={`/post/${p.id}`} className={classes.img}>
-          <img src={firstImage} alt="post" />
-        </Link>
-      )}
       <Box p={isDesktop ? 0 : 1}>
         <Link to={`/post/${p.id}`}>
           <Box className={classes.titleContainer} mb={1} display="flex">
