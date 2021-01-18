1519a096edfff1055d0e7d75d8051e56626cf273
DK <20438637+jenna1k@users.noreply.github.com>
2021-01-18 13:27:38 +0900

    검색결과 갯수 표시

--- src/components/SearchInstant.tsx
+++ src/components/SearchInstant.tsx
@@ -5,16 +5,37 @@ import {
   SearchBox,
   Pagination,
   Configure,
+  Stats,
 } from "react-instantsearch-dom";
 import { CustomHits } from "./CustomHits";
+import { makeStyles } from "@material-ui/core";
 const client = algoliasearch("6HUA4GMFJJ", "6310f1a94693ca85bf55899a686f05a5");
+const useStyles = makeStyles(theme => {
+  return {
+    searchResult: {
+      borderBottom: "1px solid " + theme.palette.divider,
+      fontSize: 20,
+      padding: theme.spacing(2),
+    },
+  };
+});
 export default function SearchInstant() {
   const { group_id } = useParams<{ group_id: string }>();
+  const classes = useStyles();
   return (
     <InstantSearch indexName="green-newdeal" searchClient={client}>
-      <h1>검색</h1>
-      <Configure hitsPerPage={4} filters={`group_id:${group_id}`} />
       <SearchBox />
+      <div className={classes.searchResult}>
+        <span>검색결과 </span>
+        <Stats
+          translations={{
+            stats(nbHits) {
+              return `${nbHits}`;
+            },
+          }}
+        />
+      </div>
+      <Configure hitsPerPage={4} filters={`group_id:${group_id}`} />
       <CustomHits />
       <Pagination />
     </InstantSearch>
