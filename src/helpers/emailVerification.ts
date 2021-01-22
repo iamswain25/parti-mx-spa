31edb634d708ae966e754f6bfe47c9f45902553b
DK <20438637+jenna1k@users.noreply.github.com>
2021-01-22 20:29:10 +0900

    url on sendVerificationEmail, verificationGuard

---
+++ src/helpers/emailVerification.ts
@@ -0,0 +1,26 @@
+import { auth } from "../config/firebase";
+import { DOMAIN } from "./options";
+
+const currentUser = auth.currentUser;
+
+export async function sendVerificationEmail() {
+  const actionCodeSettings = {
+    url: `${DOMAIN}/verification`,
+    handleCodeInApp: true,
+  };
+  await currentUser
+    ?.sendEmailVerification(actionCodeSettings)
+    .catch(err => console.warn("Verification email error", err));
+}
+
+export function verificationGuard(component: JSX.Element[]) {
+  const currentUser = auth.currentUser;
+  if (
+    currentUser !== undefined &&
+    (currentUser?.isAnonymous || currentUser?.emailVerified)
+  ) {
+    return component[0];
+  } else {
+    return component[1];
+  }
+}
