062097bbd0f85fe7a75783022ee4d5626c6f1568
jenna1k <jenna_kim1@hotmail.com>
2021-01-27 19:37:23 +0900

    #97 게시물 조회수 표시

--- src/components/BoardPostSub2.tsx
+++ src/components/BoardPostSub2.tsx
@@ -3,10 +3,12 @@ import { Post } from "../types";
 import { semanticDate } from "../helpers/datefns";
 import { Typography, Box } from "@material-ui/core";
 import useUser from "../store/useUser";
+import useDesktop from "./useDesktop";
 export default function BoardPostSub2({ post: p }: { post: Post }) {
   const [user] = useUser({ id: p.created_by });
   const { count_comment = 0, count_like = 0 } = p;
   const isVote = p.type === "vote";
+  const [isDesktop] = useDesktop();
   return (
     <Box color="grey.600">
       <Typography variant="body2">
@@ -15,6 +17,9 @@ export default function BoardPostSub2({ post: p }: { post: Post }) {
         </Box>
         <Box component="span" mr={1}>
           {semanticDate(p.created_at)}
+        </Box>
+        <Box component="span" mr={1}>
+          {isDesktop ? `조회 ${p.count_view || 0}` : p.count_view || 0}
         </Box>
         <Box component="span" mr={1}>
           댓글 {count_comment}
